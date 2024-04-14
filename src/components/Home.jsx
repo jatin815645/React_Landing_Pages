9b88bbe89db530d342f68c58bcc75ecdb5ec8ac0
jatin815645 <jitendrapatil815645@gmail.com>
2024-04-14 06:06:47 +0530

    Smiley add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
           </h1>
           <p className="primary-text">
             Healthy switcher chefs do all the prep work, like peeding, chopping
-            & marinating, so you can cook a fresh food at your home.
+            & marinating, so you can cook a fresh food at your home 😊.
           </p>
           <button className="secondary-button">
             Order Now <FiArrowRight />{" "}
